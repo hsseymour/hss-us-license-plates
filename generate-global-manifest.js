ec6f6e3da5ffeff09d12ab41e591d62e9ff9a453
Harry Seymour <harry.seymour@appius.com>
2026-03-09 18:30:10 +0000

    ++global mainfest + creation

---
+++ generate-global-manifest.js
@@ -0,0 +1,11 @@
+import fs from 'fs'
+import path from 'path'
+
+const root = './plates'
+const manifest = {}
+for (const state of fs.readdirSync(root)) {
+  const dir = path.join(root, state)
+  if (!fs.statSync(dir).isDirectory()) continue
+  manifest[state] = fs.readdirSync(dir).filter(f => f.endsWith('.webp'))
+}
+fs.writeFileSync('./manifest.json', JSON.stringify(manifest, null, 2))
