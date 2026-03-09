6ce1e0be5d1b1ee21533216038082f841f696f67
Harry Seymour <harry.seymour@appius.com>
2026-03-09 18:24:43 +0000

    create indexes + generator

---
+++ generate-index.js
@@ -0,0 +1,13 @@
+import fs from "fs";
+import path from "path";
+
+const root = "./plates";
+
+for (const state of fs.readdirSync(root)) {
+  const dir = path.join(root, state);
+  if (!fs.statSync(dir).isDirectory()) continue;
+
+  const files = fs.readdirSync(dir).filter((f) => f.endsWith(".webp"));
+  const outPath = path.join(dir, "index.json");
+  fs.writeFileSync(outPath, JSON.stringify(files, null, 2));
+}
