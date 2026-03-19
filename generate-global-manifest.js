cd11d98d71dd5a423a1f3a90ddc7855716883e38
Harry Seymour <harry.seymour@appius.com>
2026-03-19 15:06:44 +0000

    resolve encoding issue

--- generate-global-manifest.js
+++ generate-global-manifest.js
@@ -1,11 +1,17 @@
-import fs from 'fs'
-import path from 'path'
+import fs from "fs";
+import path from "path";
 
-const root = './plates'
-const manifest = {}
+const root = "./plates";
+const manifest = {};
+
 for (const state of fs.readdirSync(root)) {
-  const dir = path.join(root, state)
-  if (!fs.statSync(dir).isDirectory()) continue
-  manifest[state] = fs.readdirSync(dir).filter(f => f.endsWith('.webp'))
+    const dir = path.join(root, state);
+    if (!fs.statSync(dir).isDirectory()) continue;
+
+    manifest[state] = fs
+        .readdirSync(dir)
+        .filter((f) => f.endsWith(".webp"))
+        .map((f) => encodeURIComponent(f));
 }
-fs.writeFileSync(`${root}/manifest.json`, JSON.stringify(manifest, null, 2))
+
+fs.writeFileSync(`${root}/manifest.json`, JSON.stringify(manifest, null, 2));
--- generate-index.js
+++ generate-index.js
@@ -4,10 +4,14 @@ import path from "path";
 const root = "./plates";
 
 for (const state of fs.readdirSync(root)) {
-  const dir = path.join(root, state);
-  if (!fs.statSync(dir).isDirectory()) continue;
+    const dir = path.join(root, state);
+    if (!fs.statSync(dir).isDirectory()) continue;
 
-  const files = fs.readdirSync(dir).filter((f) => f.endsWith(".webp"));
-  const outPath = path.join(dir, "index.json");
-  fs.writeFileSync(outPath, JSON.stringify(files, null, 2));
+    const files = fs
+        .readdirSync(dir)
+        .filter((f) => f.endsWith(".webp"))
+        .map((f) => encodeURIComponent(f));
+
+    const outPath = path.join(dir, "index.json");
+    fs.writeFileSync(outPath, JSON.stringify(files, null, 2));
 }
