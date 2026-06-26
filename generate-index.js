0e339ae92ed6db53a61a795be6557d7d858f6932
Harry Seymour <harry.seymour@appius.com>
2026-06-26 14:30:38 +0100

    generate index json for plates

--- generate-index.js
+++ generate-index.js
@@ -1,17 +1,35 @@
-import fs from "fs";
-import path from "path";
+const fs = require("fs");
+const path = require("path");
 
-const root = "./plates";
+const baseDir = path.join(__dirname, "plates");
+const outputFile = path.join(__dirname, "plates/index.json");
 
-for (const state of fs.readdirSync(root)) {
-    const dir = path.join(root, state);
-    if (!fs.statSync(dir).isDirectory()) continue;
+const result = {};
 
-    const files = fs
-        .readdirSync(dir)
-        .filter((f) => f.endsWith(".webp"))
-        .map((f) => encodeURIComponent(f));
+const countries = fs
+    .readdirSync(baseDir, { withFileTypes: true })
+    .filter((d) => d.isDirectory())
+    .map((d) => d.name);
 
-    const outPath = path.join(dir, "index.json");
-    fs.writeFileSync(outPath, JSON.stringify(files, null, 2));
+for (const country of countries) {
+    const countryPath = path.join(baseDir, country);
+    result[country] = {};
+
+    const states = fs
+        .readdirSync(countryPath, { withFileTypes: true })
+        .filter((d) => d.isDirectory())
+        .map((d) => d.name);
+
+    for (const state of states) {
+        const statePath = path.join(countryPath, state);
+
+        const files = fs
+            .readdirSync(statePath)
+            .filter((f) => fs.statSync(path.join(statePath, f)).isFile());
+
+        result[country][state] = files;
+    }
 }
+
+fs.writeFileSync(outputFile, JSON.stringify(result, null, 2));
+console.log("index.json generated");
